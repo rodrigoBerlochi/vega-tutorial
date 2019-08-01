add602cd9a63f2c41abfc36f0b51183bb49a89a9
Rodrigo Berlochi <rodrigo.berlochi@endava.com>
2019-08-01 17:30:25 -0300

    basic example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
+import 'vega';
+import 'vega-lite';
+import vegaEmbed from 'vega-embed';
+
+var yourVlSpec = {
+  $schema: 'https://vega.github.io/schema/vega-lite/v2.0.json',
+  description: 'A simple bar chart with embedded data.',
+  data: {
+    values: [
+      {a: 'A', b: 28},
+      {a: 'B', b: 55},
+      {a: 'C', b: 43},
+      {a: 'D', b: 91},
+      {a: 'E', b: 81},
+      {a: 'F', b: 53},
+      {a: 'G', b: 19},
+      {a: 'H', b: 87},
+      {a: 'I', b: 52}
+    ]
+  },
+  mark: 'bar',
+  encoding: {
+    x: {field: 'a', type: 'ordinal'},
+    y: {field: 'b', type: 'quantitative'}
+  }
+};
+
 function App() {
+  useEffect(() => {
+    vegaEmbed('#vis', yourVlSpec);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div id="vis"></div>
     </div>
   );
 }
